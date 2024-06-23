1d4d8f4f7d88fb7e17f17ef257a450086834862d
Andrew Domingues <contato@drewdomi.com>
2024-06-23 02:27:06 -0300

    feat: create employee component

--- src/app/core/pages/employee/create-employee/create-employee.component.ts
+++ src/app/core/pages/employee/create-employee/create-employee.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators as V,
+} from '@angular/forms';
+import { civilStates } from '../../../utils/constands/civil-states';
 
 @Component({
-  selector: 'app-create-employee',
   standalone: true,
-  imports: [],
+  selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
-  styleUrl: './create-employee.component.scss'
+  styleUrl: './create-employee.component.scss',
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class CreateEmployeeComponent {
+  civilStates = civilStates;
 
+  constructor(private formBuilder: NonNullableFormBuilder) {}
+
+  protected createEmployee = this.formBuilder.group({
+    name: ['', [V.required, V.minLength(3), V.maxLength(80)]],
+    motherName: ['', [V.required, V.minLength(3), V.maxLength(80)]],
+    cpf: ['', [V.required, V.minLength(11), V.maxLength(11)]],
+    birthDate: ['', [V.required]],
+    civilState: ['', [V.required]],
+    admissionDate: ['', [V.required]],
+    validityDate: ['', [V.required]],
+    registration: ['', [V.required, V.minLength(7), V.maxLength(8)]],
+  });
+
+  onSubmit() {
+    console.log(this.createEmployee.value);
+  }
 }
---
+++ src/app/core/utils/constands/civil-states.ts
@@ -0,0 +1,24 @@
+import { civilState } from '../../models/enums/civilState';
+
+export const civilStates = [
+  {
+    label: 'Solteiro',
+    value: civilState.SOLTEIRO,
+  },
+  {
+    label: 'Casado',
+    value: civilState.CASADO,
+  },
+  {
+    label: 'Separado',
+    value: civilState.SEPARADO,
+  },
+  {
+    label: 'Divorciado',
+    value: civilState.DIVORCIADO,
+  },
+  {
+    label: 'Viuvo',
+    value: civilState.VIUVO,
+  },
+];
