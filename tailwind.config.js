4b495bed0f973246605aece58dacc8ed32e45339
Andrew Domingues <contato@drewdomi.com>
2024-06-23 00:19:21 -0300

    chore: add daisy UI

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    logs: false,
+    themes: ["cmyk"],
+  },
+};
